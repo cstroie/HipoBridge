acf3f7f9628e448ca52b9a4e7602b106b1410cf4
Costin Stroie <costinstroie@gmail.com>
2025-11-24 19:25:49 +0200

    fix: remove debug print statement from get_fhir_diagnostic_report

--- hipp.py
+++ hipp.py
@@ -709,7 +709,6 @@ async def get_fhir_diagnostic_report(request):
         # Return DiagnosticReport
         report_data = parse_report_data(response_text)
         report_data['report_id'] = id
-        print(report_data)
         fhir_response = create_fhir_diagnostic_report(report_data, request)
         return web.json_response(fhir_response)
 
